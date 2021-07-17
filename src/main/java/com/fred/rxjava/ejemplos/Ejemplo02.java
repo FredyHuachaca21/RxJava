4a621f3e37427c98a72140ec6e0070266a0e7f6b
Fredy <fredy.huachaca21@gmail.com>
2021-07-17 08:08:29 -0500

    Ejemplo 02 - operador MAP

--- src/main/java/com/fred/rxjava/ejemplos/Ejemplo02.java
+++ src/main/java/com/fred/rxjava/ejemplos/Ejemplo02.java
@@ -2,15 +2,13 @@ package com.fred.rxjava.ejemplos;
 
 import io.reactivex.rxjava3.core.Observable;
 
-public class Ejemplo01 {
-
+public class Ejemplo02 {
     public static void main(String[] args) {
+
         Observable<String> nombres = Observable
                 .just("Fred", "Alex", "Marco", "Frank", "Antony", "Alessia", "Ivan", "Eduardo");
 
-        /*1*/
-        nombres.subscribe(System.out::println);
-
+        /*2 MAP => OPERADOR DE TRANSFORMACION*/
+        nombres.map(String::length).subscribe(System.out::println);
     }
-
 }
